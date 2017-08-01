ef2a1fcf6813d55dde4f352ba3f5ac6e8d49bd1a
liang.zhang <liang.zhang@bestwehotel.com>
2017-08-01 14:15:24 +0800

    清理import

--- src/main/java/com/wehotel/rmq/consumer/TestQueueListener.java
+++ src/main/java/com/wehotel/rmq/consumer/TestQueueListener.java
@@ -2,8 +2,6 @@ package com.wehotel.rmq.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
 import org.springframework.stereotype.Component;
 
 /**
